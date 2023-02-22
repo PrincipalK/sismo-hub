82a35b0cf770ecd53b412d9fd1c29a9126dc53aa
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-22 16:25:19 +0100

    [factory]: Adding new hodlini group and its ZK Badge

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -142,6 +142,7 @@ import hazae41GithubFollowers from "./hazae41-github-followers";
 import helper from "./helper";
 import hennadii from "./hennadii";
 import higepens from "./higepens";
+import hodlini from "./hodlini";
 import houdzEth from "./houdz-eth";
 import hthoe from "./hthoe";
 import huuTienPlayer from "./huu-tien-player";
@@ -553,6 +554,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "helper": helper,
   "hennadii": hennadii,
   "higepens": higepens,
+  "hodlini": hodlini,
   "houdz-eth": houdzEth,
   "hthoe": hthoe,
   "huu-tien-player": huuTienPlayer,
